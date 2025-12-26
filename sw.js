05fdd41c24fd4aa5928bd20dc3af07935d29e921
RdEngenharia <rodrigues.solar@hotmail.com>
2025-12-26 15:54:54 -0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,27 @@
+const CACHE_NAME = 'rd-eng-v1';
+const assets = [
+  './',
+  './index.html',
+  './logo.png',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css',
+  'https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/jspdf-autotable/3.5.28/jspdf.plugin.autotable.min.js'
+];
+
+// Instalação e Cache
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(assets);
+    })
+  );
+});
+
+// Resposta mesmo offline
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
